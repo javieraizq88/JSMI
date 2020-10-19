cebb49473e898ecce123fe0ff58776a663f81beb
Javiera Izquierdo M <javieraizquierdo7@gmail.com>
2020-10-19 19:35:55 -0300

    carrusel

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -3,6 +3,30 @@ import React from "react";
 const Home = props =>{
     return (
         <div className="contenedorPag">
+        <div id="carouselExampleFade" className="carousel slide carousel-fade" data-ride="carousel">
+            <div className="carousel-inner">
+                <div className="carousel-item active">
+                    <img src="https://images.unsplash.com/photo-1544739313-6fad02872377?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1000&q=60"
+                        className="d-block w-100" alt="..." />
+                </div>
+                <div className="carousel-item">
+                    <img src="https://images.unsplash.com/photo-1511795409834-ef04bbd61622?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1000&q=60"
+                        className="d-block w-100" alt="..." />
+                </div>
+                <div className="carousel-item">
+                    <img src="https://images.unsplash.com/photo-1528605248644-14dd04022da1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1000&q=60"
+                        className="d-block w-100" alt="..." />
+                </div>
+            </div>
+            <a className="carousel-control-prev" href="#carouselExampleFade" role="button" data-slide="prev">
+                <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span className="sr-only">Previous</span>
+            </a>
+            <a className="carousel-control-next" href="#carouselExampleFade" role="button" data-slide="next">
+                <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                <span className="sr-only">Next</span>
+            </a>
+        </div>
             <h1>Home</h1>
 
         </div>
