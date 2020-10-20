c31e3112c16b79accc452a6e707eb9834d0b57b7
Javiera Izquierdo M <javieraizquierdo7@gmail.com>
2020-10-20 09:54:44 -0300

    iconos footer

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -4,7 +4,16 @@ const Footer = props => {
     return (
         <div className="bg-dark" id="footer">
 
-            <footer>Footer</footer>
+            <footer>
+                <div>
+                <i class="fab fa-instagram"></i> <br/>
+                <i class="fab fa-linkedin-in"></i> <br/>
+                <i class="far fa-envelope"></i> <br/>
+                <i class="fab fa-github-square"></i> <br/>
+                <i class="fab fa-twitter"></i> <br/>
+                </div>
+
+            </footer>
         </div>
     )
 }
